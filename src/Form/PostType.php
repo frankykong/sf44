d21d5b3c942ce4b62784ff7921d9aa6b2741949d
frankykong <frankykong@gmail.com>
2021-08-31 11:32:41 +0800

    modified:   config/packages/doctrine.yaml 	modified:   config/routes/annotations.yaml 	modified:   config/services.yaml 	new file:   src/Controller/.gitignore 	new file:   src/Controller/FrontEndController.php 	new file:   src/Entity/.gitignore 	new file:   src/Entity/Device.php 	new file:   src/Entity/Equipment.php 	new file:   src/Entity/Post.php 	new file:   src/Entity/Project.php 	new file:   src/Entity/Reserve.php 	new file:   src/Entity/User.php 	new file:   src/Entity/Works.php 	new file:   src/Form/DeviceType.php 	new file:   src/Form/EquipmentType.php 	new file:   src/Form/PostType.php 	new file:   src/Form/ReserveType.php 	new file:   src/Form/WorksType.php 	new file:   src/Repository/.gitignore 	new file:   src/Repository/DeviceRepository.php 	new file:   src/Repository/EquipmentRepository.php 	new file:   src/Repository/PostRepository.php 	new file:   src/Repository/ProjectRepository.php 	new file:   src/Repository/ReserveRepository.php 	new file:   src/Repository/UserRepository.php 	new file:   src/Repository/WorksRepository.php 	new file:   src/Resources/config/services.yml 	new file:   src/Resources/views/Default/index.html.twig

---
+++ src/Form/PostType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace cocose\WebBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title')
+                ->add('content')
+                ->add('status')
+                ->add('sketch')
+                ->add('img')
+                ->add('time');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'cocose\WebBundle\Entity\Post'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'cocose_webbundle_post';
+    }
+
+
+}
